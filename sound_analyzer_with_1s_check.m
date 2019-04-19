e6385a83b95c0737526c695336e495758a528910
Mahmud Ahmad <mahmudahmad@Mahmuds-MacBook-Pro.local>
2019-04-19 19:43:48 -0400

    Author: Mahmud Ahmad initial commit about 16 months too late because at the time while working on the project, I was too ashamed to push matlab code to my github. Lol.
    All this could be done in python, but I had already done it
    in matlab once, which is why I did it in matlab again for
    this project.

---
+++ sound_analyzer_with_1s_check.m
@@ -0,0 +1,68 @@
+%create a sound recording object with sampling frequency 44100, 24bits
+%quantization, and mono(1) audio
+%stero would be (2)
+sound_recorder = audiorecorder(44100, 24, 1);
+
+%vector to store recorded data in after each sample
+intermediate_sound_vector = [];
+
+%vector of last 15 samples, currently unused
+continuous_sound_vector = [];
+
+%sample counter
+%used to limit time application is run
+number_of_samples = 0;
+
+%number of times threshhold seen
+threshold_hit = 0;
+
+
+%run for 200 samples
+%if while 1, would run forever
+while number_of_samples < 150
+	%increment number of samples
+	number_of_samples = number_of_samples + 1;
+	
+	%record data for 0.01s
+	recordblocking(sound_recorder, 0.01);
+	
+	%convert sound to array of floats
+	intermediate_sound_vector = getaudiodata(sound_recorder);
+	
+	%get #of samples
+	length_of_sound_vector = length(intermediate_sound_vector);
+	%get frequency axis
+	frequency_samples = (44100/length_of_sound_vector)*[0: (1): length_of_sound_vector - 1];
+	%get FFT of sound
+	fspectrum_sound_vector = abs(fft(intermediate_sound_vector));
+	%plot spectrum of sound
+	plot(frequency_samples, reshape(fspectrum_sound_vector, [1, 441]));
+	
+	%this if checks if the fft vector contains a spike at
+    %(index - 1)*100hZ, greater than 0.2
+    %so for index = 201, we are looking for a 20kHz signal
+	if(fspectrum_sound_vector(151) > 0.1)
+		threshold_hit = threshold_hit + 1;
+        disp(threshold_hit);
+        if(threshold_hit > 10)
+            threshold_hit = 0;
+            options = weboptions('Timeout', 20);
+            webwrite("https://safety-earmuff.herokuapp.com/send-mail", "status=found", options);
+            disp("Writing to server");
+            disp("Found");
+        end
+	else
+		threshold_hit = 0;
+	end
+	
+	%if signal is found and seen for "0.03s", we alert the users,
+	%time is actually longer than 0.03s due to all data being analyzed
+	%on a single thread
+	%{
+	if(threshhold_hit == 1)
+		%make an HTTP post request to a server
+		webwrite("https://safety-earmuff.herokuapp.com/send-mail", "status=found");
+		disp("Writing to server");
+	end
+	%}
+end
